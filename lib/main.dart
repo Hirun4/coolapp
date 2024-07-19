e8213cb82037eebe5774c3e11ca4d602ad38e1c5
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-07-19 12:23:21 +0530

    debug false

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.green,
-              title: const Text('flutter is fun'),
-            ),
-            body: Container(
-                child: const Text('Hi hirun 😎'),
-                margin: const EdgeInsets.all(50),
-                padding: const EdgeInsets.all(10),
-                color: Colors.red,
-                height: 100,
-                width: 100)));
+          appBar: AppBar(
+            backgroundColor: Colors.green,
+            title: const Text('flutter is fun'),
+          ),
+          body: Container(
+              child: const Text('Hi hirun 😎'),
+              margin: const EdgeInsets.all(50),
+              padding: const EdgeInsets.all(10),
+              color: Colors.red,
+              height: 100,
+              width: 100),
+        ));
   }
 }
