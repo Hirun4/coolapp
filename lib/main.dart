18b5cf36b4d147a0eaf4d34eaa4eb6fe3c80862f
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-08-14 12:23:21 +0530

    stack practice

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
         //   ],
         // ),
         body: Stack(
-          children: const [],
+          children: const [Container(), Icon(Icons.verified)],
         ),
       ),
     );
